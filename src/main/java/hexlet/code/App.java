7745ffec29bedb94de442e5e0e7cc1f7fab0feec
Makariy <makariywallet@gmail.com>
2022-04-25 07:04:27 +0300

    init javalin app
    init javalin app

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
+
 public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.parseInt(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello"));
+    }
+
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(JavalinConfig::enableDevLogging);
+
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        addRoutes(app);
+
+        return app;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = getApp();
+        app.start(getPort());
     }
 }
